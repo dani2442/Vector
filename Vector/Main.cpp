f8c9f5d6fae65ccf177a26474a78e58e6c68f484
dani2442 <daniel.l.m.2442.123@gmail.com>
2018-05-10 21:35:55 +0200

    Initial pull

---
+++ Vector/Main.cpp
@@ -0,0 +1,5 @@
+#include "Vector.h"
+
+int main() {
+	Vector<vector<double>> hola = { {3,2},{2,3} };
+}
---
+++ Vector/Vector.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+template<typename T>
+class Vector:public vector<T>
+{
+public:
+	using vector<T>::vector;
+
+
+private:
+
+};
