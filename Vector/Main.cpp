93e08c36c0007cd0b36aa1257ba3d92959db065b
daniel lopez <daniel.l.m.2442.123@gmail.com>
2018-05-12 23:12:35 +0200

    Final

--- Vector/Main.cpp
+++ Vector/Main.cpp
@@ -1,17 +1,20 @@
 #include "Vector.h"
 
 int main() {
-	Vector<vector<double>> v1;
+	Vector<double> v1;
 	v1.v= { {3, 2}, { 4,5 }, { 3,5 } };
-	Vector<vector<double>> v2; v2.v = { { 3,2 },{ 2,3 },{3,4} };
-	Vector<vector<double>> v3;
-	Vector<vector<double>> v4;
-	Vector<int> dani; dani.v = { 3,4,5,6 };
-	
-	dani=dani*3;
+	Vector<double> v2; v2.v = { { 3,2 },{ 2,3 },{3,4} };
+	Vector<double> v3(2);
+	Vector<double> v4; v4.v = { {1,2},{3,4} };
+	v1.Print();
+	v1+= v2;
+	3*v1;
+	v1*v1;
 	v1.Check_Size();
 	v1.Print();
 	v1.Transponse();
 	v1.Print();
+	v4.Inverse();
+	v4.Print();
 	getchar();
 }
--- Vector/Vector.h
+++ Vector/Vector.h
@@ -7,30 +7,57 @@ template<class T>
 class Vector
 {
 public:
-	//Vector() :v();
-	//~Vector();
-	vector<T> v;
-	//using vector<T>::vector;
+	Vector();
+	Vector(int);
+	Vector(int, int);
+
+	~Vector();
+
+	vector<vector<T>> v;
 	
 	bool Check_Size() ;
 	template<typename T1> bool Check_Size(vector<T1>&) ;
+	template<typename T1> bool Check_Size_Fast(vector<T1>&);
 
 	void Print();
 
 	void Transponse();
 
-	Vector<T>& operator = (const vector<T>& v)
+	void Inverse();
+
+
+
+	template<class T,class Q>
+	friend vector<vector<T>> operator* (const Q , Vector<T> );
+	template<class T,class Q>
+	friend vector<vector<T>> operator* (Vector<T> , const Q );
+	template<class T>
+	friend vector<vector<T>> operator*(Vector<T>& , Vector<T>&);
+	template<typename T>
+	friend vector<vector<T>> operator+(Vector<T> &, Vector<T> &);
+	template<typename T>
+	friend vector<vector<T>> operator-(Vector<T> &, Vector<T> &);
+
+	
+
+	Vector<T>& operator = (const vector<vector<T>>& v)
 	{
 		this->v = v;
 		return *this;
 	}
 
-	template<class T> friend T operator*(const Vector<T>& lhs, const Vector<T>& rhs);
-	template<class T> friend Vector<T> operator*(const T scalar);  // vector*scalar   
-	template<class T> friend Vector<T> operator*(const T scalar, const Vector<T>& rhs);
-	//const Vector<T> &operator+(int x)const {return *this;}
-	//Vector<T> operator-(const Vector<T> &a, const Vector<T> &b);
-	//Vector<T> operator+(const Vector<T> &a, const Vector<T> &b);
+	vector<vector<T>>& operator +=( Vector<T>&v) {
+		return v + (*this);
+	}
+
+	vector<vector<T>>& operator -=(Vector<T>&v) {
+		return v - (*this);
+	}
+
+	vector<vector<T>>& operator *=(Vector<T>&v) {
+		return v * (*this);
+	}
+	
 
 private:
 
@@ -38,99 +65,111 @@ private:
 
 };
 
-template<class T>
-T operator*(const Vector<T>& lhs, const Vector<T>& rhs)  // inner product
-{
-	assert(lhs.base.size() == rhs.base.size());
 
-	Vector<T> result;
-	result.base.reserve(lhs.base.size());
-	std::transform(lhs.base.begin(), lhs.base.end(), rhs.base.begin(), std::back_inserter(result.base), std::multiplies<T>());
 
-	return result;
+template <typename T>
+Vector<T>::Vector(){
 }
 
 template<class T>
-Vector<T> operator*(const Vector<T>& lhs, const T scalar)  // vector*scalar
+inline Vector<T>::Vector(int size):
+	v(size,vector<T>(size))
 {
-	return scalar * lhs;
 }
 
 template<class T>
-Vector<T> operator*(const T scalar, const Vector<T>& rhs)  // scalar*vector
+inline Vector<T>::Vector(int size_x, int size_y):
+	v(size_x,vector<T>(size_y))
 {
-	Vector<T> result;
-	result.base.reserve(rhs.base.size());
+}
 
-	std::transform(rhs.base.begin(), rhs.base.end(), std::back_inserter(result.base), std::bind1st(std::multiplies<T>(), scalar));
-
-	return result;
+template <typename T>
+Vector<T>::~Vector() {
 }
 
 
-/*
-template<typename T1,typename T2,typename T3 = decltype(std::declval<T1>() * std::declval<T2>())>
-vector<T1> operator*( Vector<vector<T1>> a,  T2& b)
+template <class T, class Q>
+vector<vector<T>> operator* (const Q c, Vector<T> v1)
 {
-
-	for (typename Vector<T1>::iterator it1 = a.v.begin(); it1 != a.v.end(); it1++) {
-		for (typename T1::iterator it2 = (*it1).begin(); it2 != (*it1).end(); it2++) {
-			(*it2) *= b;
+	for (vector<vector<T>>::iterator it = v1.v.begin(); it != v1.v.end(); it++) {
+		for (vector<T>::iterator it2 = it->begin(); it2 != it->end(); it2++) {
+			*it2 *= c;
 		}
 	}
-	return a;
+	return v1.v;
 }
-*/
-template<typename T>
-vector<T> operator-( Vector<T> &a,  Vector<T> &b)
+
+template <class T, class Q>
+vector<vector<T>> operator* ( Vector<T> v1,const Q c)
 {
+	for (vector<vector<T>>::iterator it = v1.v.begin(); it != v1.v.end(); it++) {
+		for (vector<T>::iterator it2 = it->begin(); it2 != it->end(); it2++) {
+			*it2 *= c;
+		}
+	}
+	return v1.v;
+}
+
+template<class T>
+vector<vector<T>> operator*( Vector<T>& vector1, Vector<T>&vector2) {
 	vector<int>N1(2), N2(2);
-	if (!(a.Check_Size(N1) && b.Check_Size(N2)))
+	vector1.Check_Size_Fast(N1);
+	vector2.Check_Size_Fast(N2);
+
+	if(N1[1]!=N2[0])
 		return {};
 
-	if (is_fundamental<T>::value) {
-		if (a.v.size() != b.v.size()) {}
-			return {};
-
-	}
-	else {
-		if ((N1[0] != N2[0]) || (N1[1] != N2[1]))
-			return {};
-		vector<T>Result(N1[0], T(N1[1]));
-		for (int i = 0; i < N1[0]; i++) {
-			for (int j = 0; j < N1[1]; j++) {
-				Result[i][j] = a.v[i][j] - b.v[i][j];
+	vector<vector<T>> Result(N1[0], vector<T>(N2[1]));
+	int i = 0;
+	for (typename vector<vector<T>>::iterator it1 = Result.begin(); it1 != Result.end(); it1++, i++) {
+		int j = 0;
+		for (typename vector<T>::iterator it2 = (*it1).begin(); it2 != (*it1).end(); it2++, j++) {
+			for (int k = 0; k < N1[1]; k++) {
+				*it2 += vector1.v[i][k] * vector2.v[k][j];
 			}
 		}
-		return Result;
 	}
+	return Result;
 }
 
 template<typename T>
-vector<T> operator+( Vector<T> &a,  Vector<T> &b)
+vector<vector<T>> operator+(Vector<T> &a, Vector<T> &b)
 {
 	vector<int>N1(2), N2(2);
-	if (!(a.Check_Size(N1) && b.Check_Size(N2)))
+	if (!(a.Check_Size_Fast(N1) && b.Check_Size_Fast(N2)))
 		return {};
-	
-	if (is_fundamental<T>::value) {
-		if (a.v.size() != b.v.size()){}
-		return {};		
 
-	}
-	else {
-		if ((N1[0] != N2[0]) || (N1[1] != N2[1])) 
-			return {};
-		vector<T>Result(N1[0], T(N1[1]));
-		for (int i = 0; i < N1[0]; i++) {
-			for (int j = 0; j < N1[1]; j++) {
-				Result[i][j] = a.v[i][j] + b.v[i][j];
-			}
+	if ((N1[0] != N2[0]) || (N1[1] != N2[1]))
+		return {};
+	vector<vector<T>>Result(N1[0], vector<T>(N1[1]));
+	for (int i = 0; i < N1[0]; i++) {
+		for (int j = 0; j < N1[1]; j++) {
+			Result[i][j] = a.v[i][j] + b.v[i][j];
 		}
-		return Result;
 	}
-	
+	return Result;
+
 }
+
+
+template<typename T>
+vector<vector<T>> operator-(Vector<T> &a, Vector<T> &b)
+{
+	vector<int>N1(2), N2(2);
+	if (!(a.Check_Size_Fast(N1) && b.Check_Size_Fast(N2)))
+		return {};
+
+	if ((N1[0] != N2[0]) || (N1[1] != N2[1]))
+		return {};
+	vector<vector<T>>Result(N1[0], vector<T>(N1[1]));
+	for (int i = 0; i < N1[0]; i++) {
+		for (int j = 0; j < N1[1]; j++) {
+			Result[i][j] = a.v[i][j] - b.v[i][j];
+		}
+	}
+	return Result;
+}
+
 
 template<typename T>
 void Vector<T>::Recursive_Check(vector<T>& v1) {
@@ -144,20 +183,29 @@ bool Vector<T>::Check_Size()
 	}
 	else {
 		size_t size_y = 0;
-		for (typename vector<T>::iterator it =this->v.begin(); it != this->v.end(); ++it) {
+		for (typename vector<vector<T>>::iterator it = this->v.begin(); it != this->v.end(); ++it) {
 			if (size_y != 0) {
-				if (size_y != (*it).size())
+				if (size_y != it->size())
 				{
 					cout << "matriz invalida" << endl;
 					return false;
 				}
 			}
 			else {
-				size_y = (*it).size();
+				size_y = it->size();
 			}
 		}
 	}
 }
+template<typename T>
+template<typename T1>
+bool Vector<T>::Check_Size_Fast(vector<T1>&mysize)
+{
+	mysize[0] = this->v.size();
+	mysize[1] =this->v[0].size();
+	return true;
+}
+
 
 template<typename T>
 template<typename T1>
@@ -171,7 +219,7 @@ bool Vector<T>::Check_Size(vector<T1>&mysize)
 	T1 size_x = 0;
 	T1 size_y = 0;
 
-	for (int i = 0; i < this->v.size();i++) {
+	for (int i = 0; i < this->v.size(); i++) {
 		if (size_y != 0) {
 			if (size_y != this->v[i].size())
 			{
@@ -196,35 +244,28 @@ void Vector<T>::Print()
 	if (!this->Check_Size())
 		return;
 
-	if (!is_fundamental<T>::value) {
-		for (typename vector<T>::iterator it1 = this->v.begin(); it1 != this->v.end(); it1++) {
-			for (typename T::iterator it2 = it1->begin(); it2 != it1->end(); it2++) {
-				cout << *it2 << " ";
-			}
-			cout << endl;
+	for (typename vector<vector<T>>::iterator it1 = this->v.begin(); it1 != this->v.end(); it1++) {
+		for (typename vector<T>::iterator it2 = it1->begin(); it2 != it1->end(); it2++) {
+			cout << *it2 << " ";
 		}
 		cout << endl;
 	}
-	else {
-		for (typename vector<T>::iterator it2 = this->v.begin(); it2 != this->v.end(); it2++) {
-			cout << &it2 << " ";
-		}
-		cout << endl;
-	}
+	cout << endl;
+
 }
 
 template<typename T>
 inline void Vector<T>::Transponse()
 {
 	vector<int> mysize(2);
-	if (!this->Check_Size(mysize)) {
-		return ;
+	if (!this->Check_Size_Fast(mysize)) {
+		return;
 	}
-	vector<T> myvector2(mysize[1], T(mysize[0]));
+	vector<vector<T>> myvector2(mysize[1], vector<T>(mysize[0]));
 	int i = 0;
-	for (typename vector<T>::iterator it1 = this->v.begin(); it1 != this->v.end(); ++it1, i++) {
+	for (typename vector<vector<T>>::iterator it1 = this->v.begin(); it1 != this->v.end(); ++it1, i++) {
 		int j = 0;
-		for (typename T::iterator it2 = (*it1).begin(); it2 != (*it1).end(); ++it2, j++) {
+		for (typename vector<T>::iterator it2 = it1->begin(); it2 != it1->end(); ++it2, j++) {
 			myvector2[j][i] = *it2;
 		}
 	}
@@ -232,11 +273,70 @@ inline void Vector<T>::Transponse()
 	return;
 }
 
+template<typename T>
+inline void Vector<T>::Inverse()
+{
+	if (this->v.size() != this->v[0].size())
+	{
+		cout << "ERROR on inverse() not square array" << endl; getchar(); return;//returns a null
+	}
 
+	size_t dim = this->v.size();
+	int i, j, ord;
+	vector< vector<T> > x(dim, vector<T>(dim)), y(dim, vector<T>(dim));//output
+	x = this->v;																			  //init_2Dvector(x, dim, dim); x = x2;																			  //init_2Dvector(y, dim, dim);
+																					  //1. Unity array y: 
+	for (i = 0; i<dim; i++)
+		for (j = 0; j<dim; j++)
+		{
+			if (i == j)
+				y[i][j] = 1.0;
+			else
+				y[i][j] = 0.0;
+		}
 
+	double diagon, coef;
+	double *ptrx, *ptry, *ptrx2, *ptry2;
+	for (ord = 0; ord<dim; ord++)
+	{
+		//2 Hacemos diagonal de x =1
+		diagon = x[ord][ord];
+		if (fabs(diagon)<1e-15)
+		{
+			long i2;
+			for (i2 = ord + 1; i2<dim; i2++)
+			{
+				if (fabs(x[ord][i2])>1e-15) break;
+			}
+			if (i2 >= dim)
+				return;//error, returns null
+			for (i = 0; i<dim; i++)//sumo la linea que no es 0 el de la misma fila de ord
+			{
+				x[ord + i][ord] += x[ord + i][i2];
+			}
+		}
+		ptry = &y[ord][0];
+		for (i = 0; i<dim; i++)
+			*ptry++ /= diagon;
+		ptrx = &x[ord][ord];
+		for (i = ord; i<dim; i++)//para i<ord *ptrx=0,
+			*ptrx++ /= diagon;
 
-
-
-
-
-
+		//Hacemos '0' la columna ord salvo elemento diagonal:
+		for (i = 0; i<dim; i++)//Empezamos por primera fila
+		{
+			if (i == ord) continue;
+			coef = x[i][ord];//elemento ha hacer 0 
+			if (fabs(coef)<1e-15) continue; //si es cero se evita
+			ptry = &y[i][0];
+			ptry2 = &y[ord][0];
+			for (j = 0; j<dim; j++)
+				*ptry++ = *ptry - coef * (*ptry2++);
+			ptrx = &x[i][ord];
+			ptrx2 = &x[ord][ord];
+			for (j = ord; j<dim; j++)
+				*ptrx++ = *ptrx - coef * (*ptrx2++);
+		}
+	}//end ord
+	this->v = y;
+}
